57ba1e0283f4b2149f642974667a1f8c51bae4b4
attrenate <nikolozghomidzevol100@gmail.com>
2025-11-30 23:56:37 +0400

    pushed milestones data to new component

---
+++ react-app/src/data/milestones.js
@@ -0,0 +1,19 @@
+
+const milestones = [
+  { year: 2006, title: "Born", description: "Batumi, Georgia" },
+  {year: 2009, title: "Preschool", description: "Started preschool education."},
+  {year: 2012, title: "started school", description: "First day of primary school."},
+  {year: 2014, title: "First competition", description: "Participated in first math competition."},
+  { year: 2015, title: "Won award", description: "Won first place in regional math coontest."},
+  {year: 2018, title: "Science fair", description: "Presented project at school science fair."},
+  {year: 2019, title: "Basketball tournament", description: "2nd place in inter-school basketball tournament."},
+  { year: 2020, title: "Online learning", description: "Adapted to remote learning during pandemic." },
+  { year: 2022, title: "First hakathon", description: "Participated in local coding hakathon."},
+  { year: 2023, title: "NASA acceleration program", description: "Sent project to NASA's program with my team."},
+  { year: 2024, title: "Started coding", description: "Learning JS & React" },
+  { year: 2024, title: "Graduatuation", description: "Graduated from high-school." },
+  {year: 2024, title: "mentor Assitant", description: "Became a mentor assistant at IT school."},
+  
+];
+
+export default milestones;
